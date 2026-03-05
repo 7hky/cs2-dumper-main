3ad8d3bd386918a6b8d9b18c686d44a5b807d33a
Administrator <Administrator@KERNELOS-PC>
2026-03-05 10:14:26 +0300

    nm

--- output/networksystem_dll.cs
+++ output/networksystem_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-02-26 06:46:08.281713100 UTC
+// 2026-03-05 07:13:48.544401900 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: networksystem.dll
