537cd46997340e01bfe04046faa616e19d97890f
7hky <dekster6342@gmail.com>
2026-09-10 09:13:54 +0300

    2

--- output/animationsystem_dll.cs
+++ output/animationsystem_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-09-09 16:08:06.242122900 UTC
+// 2026-09-10 06:13:34.539371200 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: animationsystem.dll
